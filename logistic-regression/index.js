90822a69328623494355900049477b4d0433fd20
eli <ellizzabbetth@gmail.com>
2023-03-16 15:44:33 -0400

    improve logistic test

--- logistic-regression/index.js
+++ logistic-regression/index.js
@@ -1,7 +1,8 @@
 
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs')
-const loadCSV = require('../load-csv')
+const loadCSV = require('../load-csv');
+const LogisticRegression = require('./logistic-regression');
 
 
 let { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv', {
@@ -14,7 +15,19 @@ let { features, labels, testFeatures, testLabels } = loadCSV('../data/cars.csv',
             return value === 'TRUE' ? 1 : 0;
         }
     }
-
 })
 
+
+const regression = new LogisticRegression(features, labels, {
+    learningRate: 0.5,
+    iterations: 100,
+    batchSize: 50
+})
+
+regression.train();
+regression.predict(
+[[130, 307, 1.75]]
+).print()
+
+console.log('')
 console.log(labels)

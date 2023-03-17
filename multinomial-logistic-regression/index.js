fec0e9f7a56ead9183e7726c29fd96df32a1df9a
eli <ellizzabbetth@gmail.com>
2023-03-17 14:31:02 -0400

    multinomial logistic

---
+++ multinomial-logistic-regression/index.js
@@ -0,0 +1,22 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const LogisticRegression = require('./logistic-regression');
+const plot = require('node-remote-plot');
+const _ = require('lodash');
+const mnist = require('mnist-data');
+
+const mnistData = mnist.training(0, 10);
+
+
+const features = mnistData.images.values.map(image => _.flatMap(image))
+console.log(features)
+
+console.log(mnistData.labels.values)
+
+const encodedLabels = mnistData.labels.values.map(label => {
+    const row = new Array(10).fill(0);
+    row[label] = 1;
+    return row;
+})
+
+console.log(encodedLabels)
